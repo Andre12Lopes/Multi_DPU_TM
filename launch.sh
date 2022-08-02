2767c66ec96731b283aa4e24594da1a3439d4a5a
Andre12Lopes <andre.flopes@outlook.pt>
2022-08-02 19:17:35 +0100

    Fix

--- launch.sh
+++ launch.sh
@@ -1,11 +1,25 @@
 !/bin/bash
 echo -e "N_THREADS_DPU\tN_TANSACTIONS\tN_BACHES\tN_DPUS\tCOPY_TIME\tTOTAL_TIME" > results.txt
 
-for (( i = 1; i <= 10000; i *= 10 )); do
+make clean
+make test NR_BACHES=10 NR_TANSACTIONS=1000 NR_ACCOUNTS=800 NR_DPUS=1
+
+for (( j = 0; j < 10; j++ )); do
+	./host/host >> results.txt
+done
+
+for (( i = 500; i <= 2500; i += 500 )); do
 	make clean
-	make test NR_TANSACTIONS=$i NR_ACCOUNTS=800 NR_DPUS=10
+	make test NR_BACHES=10 NR_TANSACTIONS=1000 NR_ACCOUNTS=800 NR_DPUS=$i
 	
 	for (( j = 0; j < 10; j++ )); do
 		./host/host >> results.txt
 	done
+done
+
+make clean
+make test NR_BACHES=10 NR_TANSACTIONS=1000 NR_ACCOUNTS=800 NR_DPUS=2554
+
+for (( j = 0; j < 10; j++ )); do
+	./host/host >> results.txt
 done
